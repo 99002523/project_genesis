6048aabe58c86305369b29f99f4d6f2862a064bd
99002523 <prasanna.a@ltts.com>
2020-09-18 22:36:13 +0530

    basic

---
+++ inc/rsa.h
@@ -0,0 +1,9 @@
+#ifndef __ADDTHEMALL_H__
+#define __ADDTHEMALL_H__
+
+void encrypt();
+void decrypt();
+long int findGCD(long int,long int);
+long int multipicative_inverse(long int,long int);
+
+#endif
